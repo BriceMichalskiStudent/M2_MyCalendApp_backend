a52a85934002c50979188e7cf3a7652f5c6ef15a
BouleWayne <benjaminlhonnen@gmail.com>
2020-12-17 15:10:35 +0100

    modification des location

--- Repository/EventRepository.ts
+++ Repository/EventRepository.ts
@@ -1,11 +1,4 @@
 import CRUDRepository from "../Core/Repository/CrudRepository";
 
 export default class EventRepository extends CRUDRepository {
-    async beforeInsert(data: any): Promise<any> {
-        let eventCopy = Object.assign({}, data);
-        eventCopy.location = {
-            "type": "Point", "coordinates": data.location
-        };
-        return super.beforeInsert(eventCopy);
-    }
 }
